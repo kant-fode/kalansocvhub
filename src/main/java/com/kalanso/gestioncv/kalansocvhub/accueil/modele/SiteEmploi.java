adbec6cc75fa3a4787743647ef7058ead774198d
kante.fode <fodekanteodc@gmail.com>
2023-11-13 22:28:54 +0000

    revision1

--- src/main/java/com/kalanso/gestioncv/kalansocvhub/accueil/modele/SiteEmploi.java
+++ src/main/java/com/kalanso/gestioncv/kalansocvhub/accueil/modele/SiteEmploi.java
@@ -7,31 +7,8 @@ import lombok.Data;
 @Entity
 public class SiteEmploi {
     private String titre;
-    // sa c'est la classe private je vais creer mais accesseurs pour pouvoir
-    // y accedez un peu plus tard
-    public String getTitre() {
-        return titre;
-    }
-    public void setTitre(String titre) {
-        this.titre = titre;
-    }
-    // sa c'est la classe private je vais creer mais accesseurs pour pouvoir
-    // y accedez un peu plus tard pour date publication
     private String datePublication;
-    public String getDatePublication() {
-        return datePublication;
-    }
-    public void setDatePublication(String datePublication) {
-        this.datePublication = datePublication;
-    }
-    // sa c'est la classe private je vais creer mais accesseurs pour pouvoir
-    // y accedez un peu plus tard pour lieu
     private String lieu;
-    public String getLieu() {
-        return lieu;
-    }
-    public void setLieu(String lieu) {
-        this.lieu = lieu;
-    }
+
     
 }
--- src/main/java/com/kalanso/gestioncv/kalansocvhub/accueil/modele/utilisateur.java
+++ src/main/java/com/kalanso/gestioncv/kalansocvhub/accueil/modele/utilisateur.java
@@ -9,22 +9,7 @@ import lombok.Data;
 
 public class utilisateur {
     private String nom;
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
     private String mail;
-    public String getMail() {
-        return mail;
-    }
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
     private String motdepasse;
-    public String getMotdepasse() {
-        return motdepasse;
-    }
-    public void setMotdepasse(String motdepasse) {
-        this.motdepasse = motdepasse;
-    }
-    
+
 }
